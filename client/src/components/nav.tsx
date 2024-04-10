5ef6ec54c998c7c2570e5ed3fe9386664d573607
Athul Anoop <athulanoop2020@gmail.com>
2024-04-10 12:12:55 +0530

    Update nav.tsx

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -1,23 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Menu } from "lucide-react";
+
+const navVariants = {
+  navbar:
+    "absolute -top-24 flex justify-evenly items-center gap-16 opacity-0 sm:static sm:opacity-100 ease-in-out duration-500",
+  twNavrailBase:
+    "w-2/5 p-4 flex flex-col justify-evenly items-center gap-6 rounded-lg bg-white ring ring-black text-left ease-in-out duration-500",
+  twNavrailVisible: "absolute top-14 right-5 ",
+  twNavrailHidden: "absolute top-14 -right-full",
+};
+
+const navItems = [
+  { title: "Home", url: "/" },
+  { title: "Portfolio", url: "/portfolio" },
+  { title: "Blog", url: "/blog" },
+];
+
 const Nav = () => {
+  const [navrailHidden, setNavrailHidden] = useState(true);
+  const toggleNavrail = () => {
+    setNavrailHidden(() => !navrailHidden);
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 640) {
+        setNavrailHidden(true);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
   return (
-    <nav className="border w-full h-12 px-4 flex justify-between items-center flex-shrink-0 bg-white z-20">
+    <nav className="border w-full h-12 px-4 flex justify-between items-center flex-shrink-0 bg-white z-20 ">
       <h1 className="text-lg font-bold">Athul Anoop</h1>
-      <ul className="absolute -top-24 flex justify-evenly items-center gap-16 opacity-0 sm:static sm:opacity-100 ease-in-out duration-500">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="/blog">Blog</a>
-        </li>
+      {navrailHidden && (
+        <ul className={navVariants.navbar}>
+          {navItems.map((item, index) => (
+            <li key={index} className="w-full">
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+      <ul
+        className={
+          navrailHidden
+            ? `${navVariants.twNavrailBase} ${navVariants.twNavrailHidden}`
+            : `${navVariants.twNavrailBase} ${navVariants.twNavrailVisible}`
+        }
+      >
+        {navItems.map((item, index) => (
+          <li key={index} className="w-full flex shadow-sm rounded-lg border">
+            <a className="w-full p-2" href={item.url}>
+              {item.title}
+            </a>
+          </li>
+        ))}
       </ul>
-      <span className="static opacity-100 sm:absolute sm:opacity-0 ease-in-out duration-500">
+      <button
+        className="static opacity-100 sm:absolute sm:opacity-0 ease-in-out duration-500"
+        onClick={toggleNavrail}
+      >
         <Menu />
-      </span>
+      </button>
     </nav>
   );
 };
