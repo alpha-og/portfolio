7e5e01e3a8b0b1fb7bbabfea9633d7d4a916e986
Athul Anoop <athulanoop2020@gmail.com>
2024-04-10 14:18:15 +0530

    Update components.ts

--- client/src/components/components.ts
+++ client/src/components/components.ts
@@ -1,6 +1,15 @@
 import Nav from "./nav";
 import ProfileCard from "./home/profile-card";
 import ContactCard from "./home/contact-card";
-import TechstackCard from "./home/techstack-card";
+import SkillsCard from "./portfolio/skills-card";
+import ProjectsCard from "./portfolio/projects-card";
+import InterestsCard from "./portfolio/interests-card";
 
-export { Nav, ProfileCard, ContactCard, TechstackCard };
+export {
+  Nav,
+  ProfileCard,
+  ContactCard,
+  SkillsCard,
+  ProjectsCard,
+  InterestsCard,
+};
