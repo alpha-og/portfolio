5024e955197bcc3357a50145338db228b79c43b0
Athul Anoop <athulanoop2020@gmail.com>
2025-03-12 22:14:25 +0530

    update: footer completed

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,5 +1,27 @@
 export default function Footer() {
   return (
-    <footer className="h-80 bg-white/20 ring-white/20 backdrop-blur-sm border-2 border-white/20"></footer>
+    <footer className="h-max px-4 py-4 flex justify-between bg-white/20 ring-white/20 backdrop-blur-sm border-2 border-white/20 sm:px-8 xl:px-16">
+      <div className="w-max flex gap-2 flex-nowrap text-sm text-black/80 md:text-base xl:text-lg">
+        <span>&copy; {new Date().getFullYear()} | </span>
+        <a
+          href="https://github.com/alpha-og"
+          target="_blank"
+          rel="noopener noreferrer"
+          className=""
+        >
+          Athul Anoop
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://github.com/alpha-og/portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm text-black/80 md:text-base xl:text-lg hover:text-black/100 transition-all duration-300 ease-in-out"
+        >
+          View on GitHub
+        </a>
+      </div>
+    </footer>
   );
 }
